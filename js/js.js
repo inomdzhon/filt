4e32730652fbcdfdf563f50334e2cceb8945c47c
Rafael <rafael_guliev94@list.ru>
2018-07-13 02:22:07 +0300

    wtf

--- js/js.js
+++ js/js.js
@@ -1,23 +1,4 @@
-// var goods = document.querySelectorAll('.good');
-var goods = [
-  {
-   'id' : '1',
-   'color' : 'white',
-   'size' : '44'
-  },
-  {
-    'id' : '2',
-    'color' : 'black',
-    'size' : '44'
-  },
-  {
-    'id' : '3',
-    'color' : 'brown',
-    'size' : '43'
-  }
-];
-function getFilteredByColor(array, color) {
-    return array.filter(function (item) {
-      return item.color === color;
-  });
-}
+  var check = document.querySelector('input[type="checkbox"]');
+  check.addEventListener('change', function () {
+      document.querySelector('.good--black').style.display = 'none'
+    })
