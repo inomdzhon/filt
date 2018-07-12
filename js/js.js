ec9f2dc34be448abab2768a9e2946c50d67a8768
Rafael <rafael_guliev94@list.ru>
2018-07-12 14:42:35 +0300

    фыв

--- js/js.js
+++ js/js.js
@@ -1,2 +1,21 @@
 var goods = document.querySelectorAll('.good');
+goods = [
+  {
+   'id' : '1',
+   'color' : 'white',
+   'size' : '44'
+  },
+  {
+    'id' : '2',
+    'color' : 'black',
+    'size' : '44'
+  },
+  {
+    'id' : '3',
+    'color' : 'brown',
+    'size' : '43'
+  }
+]
 console.log(goods.length);
+for (var i = 0; i < goods.length; i++) {
+}
