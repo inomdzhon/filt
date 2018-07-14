58ad2d67e501949735d758db6e0a08503ad4b496
Rafael <rafael_guliev94@list.ru>
2018-07-14 22:35:02 +0300

    filter lastх[?] fix

--- js/js.js
+++ js/js.js
@@ -1,4 +1,26 @@
-  var check = document.querySelector('input[type="checkbox"]');
-  check.addEventListener('change', function () {
-      alert('wtf');
-    })
+var selec = document.getElementById('sel');
+var goods = document.getElementById('goods');
+selec.onchange = function () {
+  var good = goods.getElementsByClassName('good');
+    for (var i = 0; i < good.length; i++) {
+      if (good[i].classList.contains(this.value)) {
+        good[i].style.display = 'inline-block';
+      } else {
+        good[i].style.display = 'none';
+    }
+  }
+}
+// var filter_select_el = document.getElementById('filter');
+// var items_el = document.getElementById('items');
+
+// filter_select_el.onchange = function() {
+//   console.log(this.value);
+//   var items = items_el.getElementsByClassName('item');
+//   for (var i=0; i<items.length; i++) {
+//     if (items[i].classList.contains(this.value)) {
+//       items[i].style.display = 'block';
+//     } else {
+//       items[i].style.display = 'none';
+//     }
+//   }
+// };
