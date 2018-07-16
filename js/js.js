e433a4e743e2a1ba4099d18e80919cb0508663f8
Inomdzhon Mirdzhamolov <i.mirdzhamolov@MacBook-Pro-Inomdzhon.local>
2018-07-16 22:30:35 +0300

    Refactor and extend filter

--- js/js.js
+++ js/js.js
@@ -1,37 +1,46 @@
-var selec = document.getElementById('sel');
-var goods = document.getElementById('goods');
-selec.onchange = function () {
-  let good = goods.getElementsByClassName('good');
-    for (var i = 0; i < good.length; i++) {
-      if (good[i].classList.contains(this.value)) {
-        good[i].style.display = 'inline-block';
-      } else {
-        good[i].style.display = 'none';
-    }
-  }
-}
-var size = document.getElementById('size');
-size.onchange = function () {
-  let good = goods.getElementsByClassName('good');
-    for (var i = 0; i < good.length; i++) {
-      if (good[i].classList.contains(this.value)) {
-        good[i].style.display = 'inline-block';
-      } else {
-        good[i].style.display = 'none';
-    }
-  }
-}
-// var filter_select_el = document.getElementById('filter');
-// var items_el = document.getElementById('items');
+var filters = {
+  color: false,
+  size: false,
+};
+var elemsFilter = document.querySelectorAll('.js-filter'); // <-- https://learn.javascript.ru/searching-elements-dom
+var elemGoods = document.querySelector('.js-goods');
+var elemsGoodsItem = elemGoods.querySelectorAll('.js-goods-item');
 
-// filter_select_el.onchange = function() {
-//   console.log(this.value);
-//   var items = items_el.getElementsByClassName('item');
-//   for (var i=0; i<items.length; i++) {
-//     if (items[i].classList.contains(this.value)) {
-//       items[i].style.display = 'block';
-//     } else {
-//       items[i].style.display = 'none';
-//     }
-//   }
-// };
+elemsFilter.forEach(function(elemFilter) { // <-- https://learn.javascript.ru/array-iteration#foreach
+  elemFilter.addEventListener('change', onFilterChanged); // <-- https://learn.javascript.ru/introduction-browser-events#addeventlistener-%D0%B8-removeeventlistener
+});
+
+function onFilterChanged(event) { // <-- https://learn.javascript.ru/obtaining-event-object
+  var elemFilter = event.target;
+  var type = elemFilter.dataset.type; // <-- https://learn.javascript.ru/attributes-and-custom-properties#%D1%81%D0%B2%D0%BE%D0%B9%D1%81%D1%82%D0%B2%D0%BE-dataset-data-%D0%B0%D1%82%D1%80%D0%B8%D0%B1%D1%83%D1%82%D1%8B
+  var value = elemFilter.value;
+
+  filters[type] = value === 'all' ? false : value;
+
+  filterGoods();
+}
+
+function filterGoods() {
+  elemsGoodsItem.forEach(function(elemGoodItem) {
+    var isValidItem = Object.keys(filters) // <-- https://learn.javascript.ru/array-methods#object-keys-obj
+      .every(function(filterKey) { // <-- Метод *.every https://learn.javascript.ru/array-iteration#every-some
+        var filterValue = filters[filterKey];
+
+        if (!filterValue) { // или по-другому filterValue === false
+          return true;
+        }
+
+        var goodItemValue = elemGoodItem.dataset[filterKey].split(','); // <-- https://learn.javascript.ru/array-methods#%D0%BC%D0%B5%D1%82%D0%BE%D0%B4-split
+
+        return goodItemValue.some(function(value) { // <-- Метод *.some https://learn.javascript.ru/array-iteration#every-some
+          return filterValue === value;
+        });
+      });
+
+    if (isValidItem) {
+      elemGoodItem.classList.remove('good_state_hide'); // <-- https://learn.javascript.ru/attributes-and-custom-properties#%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D1%8B-%D0%B2-%D0%B2%D0%B8%D0%B4%D0%B5-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%B0-classlist
+    } else {
+      elemGoodItem.classList.add('good_state_hide');
+    }
+  });
+}
